f2fc8e021de4d7acca7c75bec71d3527a43b1ec8
dlytv <dlytv@softserveinc.com>
2018-03-24 14:28:25 +0200

    Initial commit

---
+++ CMakeFiles/BelyashDB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BelyashDB.dir/main.cpp.o"
+  "BelyashDB.pdb"
+  "BelyashDB"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BelyashDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
